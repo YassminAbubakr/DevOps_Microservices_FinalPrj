ddb36dc05540dc0a9dae3d4543313f913c13ca93
YassminAbubakr <yassminabubakr106@gmail.com>
2021-09-28 15:36:25 +0200

    add circleci and run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag=projdocker .
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -it projdocker bash 
