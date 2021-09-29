543c1b9e770a754e1717c28e3b92b78b9e1a9a95
EC2 Default User <ec2-user@ip-172-31-31-8.ec2.internal>
2021-09-29 12:11:03 +0000

    add dockerfile , modify app.py ,making the deployment

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=projdocker .
+docker build --tag=devops_microservice .
 # Step 2: 
 # List docker images
 docker image ls
 # Step 3: 
 # Run flask app
-docker run -it projdocker bash 
+docker run -it devops_microservice bash 
