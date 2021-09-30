ce3626b05962ff629bd9e683446fc0c2550ee21f
EC2 Default User <ec2-user@ip-172-31-31-8.ec2.internal>
2021-09-30 00:12:37 +0000

    Task 4: Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="yassminabubakr/devops_microservice"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=yassminabubakr &&\
+    docker image tag devops_microservice $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
