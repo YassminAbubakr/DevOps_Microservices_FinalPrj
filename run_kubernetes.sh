1825c5dd0ac532b59716f5c707bfb9d14d0ad164
EC2 Default User <ec2-user@ip-172-31-31-8.ec2.internal>
2021-09-30 00:43:16 +0000

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="yassminabubakr/devops_microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- run devopsmicroservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl port-forward devopsmicroservice 8000:80
